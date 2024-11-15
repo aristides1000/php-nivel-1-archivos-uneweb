580faa1de2c08332491c9fd14f6d0a7895c6e9be
aristides1000 <aristides1000@gmail.com>
2024-11-14 20:50:45 -0400

    first commit

---
+++ tema-3/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Estructuras de control</title>
+</head>
+<body>
+  <ol>
+    <li><a href="./if-else.php">If else</a></li>
+    <li><a href="./elseif.php">elseif</a></li>
+    <li><a href="./while.php">while</a></li>
+    <li><a href="./do-while.php">do while</a></li>
+    <li><a href="./for.php">for</a></li>
+    <li><a href="./switch.php">switch</a></li>
+  </ol>
+</body>
+</html>
